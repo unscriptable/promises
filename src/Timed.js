64500d8309815f107c0efe9bf7e2ad00c38a050c
unscriptable <john.hann@e-numera.com>
2011-02-10 15:36:30 -0500

    makeTimed now allows a custom argument to be sent to reject() is the timeout occurs. also, it now stops the setTimeout if resolve() is called (oops)

--- src/Timed.js
+++ src/Timed.js
@@ -2,25 +2,36 @@
 // specified number of msec. Usage:
 // var myPromise = makeTimed(new Promise(), 500);
 
-function makeTimed (promise, msec) {
+function makeTimed (promise, msec, timeoutArg) {
 
 	// this function will get called if the stated msec elapse
 	function timedOut () {
-		promise.reject(new Error('Promise timed out.'));
+		var arg = typeof timeoutArg === ' undefined' ?
+				new Error('Promise timed out.') :
+				timeoutArg;
+		promise.reject(arg);
 	}
 
 	var
 		// start the timeout period
 		handle = setTimeout(timedOut, msec),
 		// grab a reference to the promise's original reject method
-		prevReject = promise.reject;
+		prevReject = promise.reject,
+		prevResolve = promise.resolve;
 
 	// create the new reject method
-	promise.reject = function (ex) {
+	promise.reject = function reject (ex) {
 		// stop the timeout
 		clearTimeout(handle);
 		// call the original reject method
 		prevReject.call(promise, ex);
+	};
+
+	promise.resolve = function resolve (val) {
+		// stop the timeout
+		clearTimeout(handle);
+		// call the original reject method
+		prevResolve.call(promise, val);
 	};
 
 	return promise;
